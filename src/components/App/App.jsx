8b81a9cbe674a55a37256df388e21deaebb71aa1
Deen Choudhury <deenchoudhury@Deens-MacBook-Pro.local>
2020-09-24 17:53:27 -0400

    Added Bootstrap dependencies

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+// import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
+    	<button className="btn btn-lg btn-block btn-outline-primary">HEllo</button>
     </div>
   );
 }
