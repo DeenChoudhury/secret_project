c732e6eb50287fa6a10a54db0585b6b1c43b9c73
Deen Choudhury <deenchoudhury@Deens-MacBook-Pro.local>
2020-09-24 20:47:35 -0400

    Added Bootstrap Navbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
+import Home from '../../pages/home/Home';
 // import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-    	<button className="btn btn-lg btn-block btn-outline-primary">HEllo</button>
-      <div></div>
-    </div>
-  );
+class App extends React.Component{
+	render(){
+		return (
+			<div className="App">
+				<BrowserRouter>
+					<Switch>
+						<Route path="/" component ={Home}/>
+					</Switch>
+				</BrowserRouter>
+   			</div>
+		)
+	}
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
+class Navbar extends React.Component{
+    render(){
+        return(
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <Link to="/" className="navbar-brand"> Rootine</Link>
+            <div class="collapse navbar-collapse" id="navbarsExample03">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <Link to="/meh" className="nav-link"> Hello</Link>
+                    </li>
+                </ul>
+          </div>
+        </nav>
+    )};
+}
+
+export default Navbar;
