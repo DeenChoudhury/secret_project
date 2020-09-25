c2f7f2aaabc4212be08f8c0b9c1fba43653e6870
Jeff So <jeffso9820@gmail.com>
2020-09-25 10:24:35 -0400

    changed colour of sidebar and added additional css styling

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 
 function Sidebar() {
     return (
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse hidden-xs-down">
-            <div class="sidebar-sticky pt-3">
+        <nav id="sidebarMenu" class="sidebar">
+            <div class="sidebar-menu">
                 <ul class="nav flex-column">
+                    <li class="first-nav-item"></li>
                     <li class="nav-item">
                         <a class="nav-link active" href="#">
                             Dashboard
@@ -39,33 +40,11 @@ function Sidebar() {
                     </li>
                 </ul>
 
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <h6 class="sidebar-title2">
                 <span>Saved reports</span>
                 <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
                 </a>
             </h6>
-                <ul class="nav flex-column mb-2">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Current month
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Last quarter
-                         </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Social engagement
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Year-end sale
-                         </a>
-                    </li>
-                </ul>
             </div>
         </nav>
     )
