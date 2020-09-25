34177c9801b1e09028340b298b928b22696ba6e4
Deen Choudhury <deenchoudhury@Deens-MacBook-Pro.local>
2020-09-25 11:19:29 -0400

    Added slide down animation to hobby form

--- src/components/HobbyForm/index.jsx
+++ src/components/HobbyForm/index.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import './styles.css'
+import './styles.css';
+import add_button from 'images/add_button.svg';
 
 function HobbyForm(){
-	return (
-		<div className="form-container">
+	const [showHobbyForm, setShowHobbyForm] = useState(false);
 
+	return (
+		<div>
+			<div className="add-hobby-container">
+				<div className="btn-container" onClick={() => setShowHobbyForm(!showHobbyForm)}>
+					<img className={showHobbyForm ? "btn-container__img--animate":"btn-container__img"} 
+					src={add_button} alt={add_button}/>
+				</div>
+				
+				<div className={showHobbyForm ? "form-container--animate":"form-container--hidden"}>
+					<h1>YERRRRRRRRR</h1>
+				</div>
+			</div>
 		</div>
 	);
 
