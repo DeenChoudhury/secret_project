eda151a0ad35ecbcdaa46e69f0d4c4d9fda78e29
Jeff So <jeffso9820@gmail.com>
2020-09-26 15:33:16 -0400

    updated sidebar CSS

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
 import Home from 'pages/Home';
+
 // import Button from 'react-bootstrap/Button';
 
 class App extends React.Component{
@@ -13,8 +14,8 @@ class App extends React.Component{
 						<Route path="/" component ={Home}/>
 					</Switch>
 				</BrowserRouter>
-   			</div>
-		)
+   		</div>
+		);
 	}
 }
 
